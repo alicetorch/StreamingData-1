f274a8ef2d13d7b751bc310b37733f38daaba186
samcbehrens <samcbehrens@gmail.com>
2016-07-06 09:48:09 -0400

    added start condition function

--- public/modules/conditions/d1/d1.js
+++ public/modules/conditions/d1/d1.js
@@ -12,30 +12,27 @@ init = function(){
 
 	if(slice == 1){
 		d3.json("modules/conditions/d1/spd1/d1Vars1.json", function(error, data){
-			console.log(data.vars.className);
-			experimentr.startTimer(data.vars.className);
-			general.setPageVars(data.vars.className);
-			component.createGraphViewer(data.vars.className);
-	    	component.addGraph(data.vars.duration,data.vars.dataPath1, data.vars.dataPath2, data.vars.dataPath3);
+			startCondition(data.vars.className, data.vars.dataPath1, data.vars.dataPath2, data.vars.dataPath3, data.vars.duration);
 		})
 	}else if(slice == 2){
 		d3.json("modules/conditions/d1/spd2/d1Vars2.json", function(error, data){
-			console.log(data.vars.className);
-			experimentr.startTimer(data.vars.className);
-			general.setPageVars(data.vars.className);
-			component.createGraphViewer(data.vars.className);
-	    	component.addGraph(data.vars.duration,data.vars.dataPath1, data.vars.dataPath2, data.vars.dataPath3);
+			startCondition(data.vars.className, data.vars.dataPath1, data.vars.dataPath2, data.vars.dataPath3, data.vars.duration);
 		})
 
  	}else{
- 			d3.json("modules/conditions/d1/spd3/d1Vars3.json", function(error, data){
-			console.log(data.vars.className);
-			experimentr.startTimer(data.vars.className);
-			general.setPageVars(data.vars.className);
-			component.createGraphViewer(data.vars.className);
-	    	component.addGraph(data.vars.duration,data.vars.dataPath1, data.vars.dataPath2, data.vars.dataPath3);
+ 		d3.json("modules/conditions/d1/spd3/d1Vars3.json", function(error, data){
+			startCondition(data.vars.className, data.vars.dataPath1, data.vars.dataPath2, data.vars.dataPath3, data.vars.duration);
 		})
 	}
+
+
+	startCondition(className, path1, path2, path3, duration){
+		console.log(className);
+		experimentr.startTimer(className);
+		general.setPageVars(className);
+		component.createGraphViewer(className);
+    	component.addGraph(duration, dataPath1, dataPath2, dataPath3);
+	};
 
 	general.countdown( "countdown", 5, 0 );
 	
